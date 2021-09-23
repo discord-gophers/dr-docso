d26ce8b160693a3bc7a23aa0df3f2e914c69a561
Sven Wiltink <sven@wiltink.dev>
2021-09-23 13:24:21 +0200

    use a json list for guild ids in the config

--- config.go
+++ config.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 
 	"github.com/diamondburned/arikawa/v3/discord"
 )
@@ -19,20 +20,53 @@ type configuration struct {
 	Blacklist map[discord.Snowflake]struct{} `json:"blacklist"`
 }
 
+// snowflakeLookup transforms a json list to a map for faster lookups
+type snowflakeLookup map[discord.Snowflake]struct{}
+
+func (c *snowflakeLookup) UnmarshalJSON(data []byte) error {
+	snowflakes := make([]discord.Snowflake, 0)
+
+	err := json.Unmarshal(data, &snowflakes)
+	if err != nil {
+		return err
+	}
+
+	*c = map[discord.Snowflake]struct{}{}
+
+	for _, s := range snowflakes {
+		(*c)[s] = struct{}{}
+	}
+
+	return nil
+}
+
+func (c snowflakeLookup) MarshalJSON() ([]byte, error) {
+	snowflakes := make([]discord.Snowflake, 0, len(c))
+	for snowflake, _ := range c {
+		snowflakes = append(snowflakes, snowflake)
+	}
+
+	sort.Slice(snowflakes, func(i, j int) bool {
+		return snowflakes[i] < snowflakes[j]
+	})
+
+	return json.Marshal(snowflakes)
+}
+
 type commandPermissions struct {
-	Docs   map[discord.RoleID]struct{}                        `json:"docs"`
-	Config map[discord.GuildID]map[discord.Snowflake]struct{} `json:"config"`
+	Docs   snowflakeLookup                     `json:"docs"`
+	Config map[discord.GuildID]snowflakeLookup `json:"config"`
 }
 
 func config() configuration {
-	var config configuration
 	fileBytes, err := os.ReadFile("config.json")
 	if err != nil {
 		log.Fatal(fmt.Errorf("could not open config, %w", err))
 	}
-	err = json.Unmarshal(fileBytes, &config)
+
+	config, err := configFromBytes(fileBytes)
 	if err != nil {
-		log.Fatal(fmt.Errorf("could not parse config, %w", err))
+		log.Fatalf("could not parse config, %s", err)
 	}
 
 	if config.Aliases == nil {
@@ -43,6 +77,23 @@ func config() configuration {
 	}
 
 	return config
+}
+
+func configFromBytes(data []byte) (configuration, error) {
+	var config configuration
+	err := json.Unmarshal(data, &config)
+	if err != nil {
+		return config, err
+	}
+
+	if config.Aliases == nil {
+		config.Aliases = map[string]string{}
+	}
+	if config.Blacklist == nil {
+		config.Blacklist = map[discord.Snowflake]struct{}{}
+	}
+
+	return config, nil
 }
 
 func saveConfig(config configuration) error {
---
+++ config_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSnowflakeLookup_MarshalJSON(t *testing.T) {
+	lookup := snowflakeLookup{
+		discord.Snowflake(1337): struct{}{},
+		discord.Snowflake(42): struct{}{},
+		discord.Snowflake(777): struct{}{},
+	}
+
+	d, err := json.Marshal(lookup)
+	assert.NoError(t, err)
+	assert.EqualValues(t, `["42","777","1337"]`, string(d))
+}
+
+func TestSnowflakeLookup_UnmarshalJSON(t *testing.T) {
+	lookup := make(snowflakeLookup)
+	input := []byte(`["1337","42","777"]`)
+
+	err := json.Unmarshal(input, &lookup)
+	assert.NoError(t, err)
+
+	expected := snowflakeLookup{
+		discord.Snowflake(1337): struct{}{},
+		discord.Snowflake(42): struct{}{},
+		discord.Snowflake(777): struct{}{},
+	}
+
+	assert.Equal(t, expected, lookup)
+
+}
+
+func TestConfigFromBytes(t *testing.T) {
+	input := []byte(`
+{
+	"prefix": "dr.",
+	"token": "--",
+	"permissions": {
+		"docs": [
+			"1337"
+		],
+		"config": {
+			"42": [
+				"777"
+			]
+		}
+	}
+}
+`)
+
+	config, err := configFromBytes(input)
+	assert.NoError(t, err)
+
+	expected := configuration{
+		Prefix:      "dr.",
+		Token:       "--",
+		Permissions: commandPermissions{
+			Docs: map[discord.Snowflake]struct{}{
+				1337: {},
+			},
+			Config: map[discord.GuildID]snowflakeLookup{
+				42: map[discord.Snowflake]struct{}{
+					777: {},
+				},
+			},
+		},
+		Aliases: map[string]string{},
+		Blacklist: map[discord.Snowflake]struct{}{},
+	}
+
+	assert.Equal(t, expected, config)
+}
